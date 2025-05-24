6feed0bce412e44d6efa9bd419f0f0eba2a6a4bd
Laila Maria da Silva <laila.silva.profissionaltext@gmail.com>
2025-05-24 00:58:54 -0300

    Create script.js

---
+++ src/assets/js/script.js
@@ -0,0 +1,25 @@
+
+const form = document.getElementById('form-login');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    if (email === '' || senha === '') {
+        alert('Preencha todos os campos!');
+        return;
+    }
+
+    if (!email.includes('@')) {
+        alert('E-mail inválido!');
+        return;
+    }
+
+    if (senha.length < 8) {
+        alert('A senha deve conter pelo menos 8 caracteres!');
+        return;
+    }
+    window.location.href = 'forum.html';
+});
