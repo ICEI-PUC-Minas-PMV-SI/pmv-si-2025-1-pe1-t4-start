3b330341b593e3263a18c4f8ab9e2ac91849f131
Laila Maria da Silva <laila.silva.profissionaltext@gmail.com>
2025-05-24 01:06:08 -0300

    Create profissional.js

---
+++ src/assets/js/profissional.js
@@ -0,0 +1,30 @@
+
+const form = document.getElementById('form-login');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const senha = document.getElementById('senha').value;
+  const confirmaSenha = document.getElementById('confirmar').value;
+
+  if (email === '' || senha === '' || confirmaSenha === '') {
+    alert('Preencha todos os campos!');
+    return;
+  }
+
+  if (!email.includes('@')) {
+    alert('E-mail inválido!');
+    return;
+  }
+
+  if (senha.length < 8) {
+    alert('A senha deve conter pelo menos 8 caracteres!');
+    return;
+  }
+
+  if (senha !== confirmaSenha) {
+    alert('As senhas não coincidem!');
+    return;
+  }
+
+  window.location.href = 'cadastroprof.html';
+});
