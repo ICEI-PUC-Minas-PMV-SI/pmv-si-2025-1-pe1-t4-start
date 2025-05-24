21399720ddb6a63be91137dbec73181daf496eb3
Laila Maria da Silva <laila.silva.profissionaltext@gmail.com>
2025-05-24 13:59:37 -0300

    Create codigo.js

---
+++ src/assets/js/codigo.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('form-login');
+const codigoInput = document.getElementById('codigo');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  if (codigoInput.value.length < 4) {
+    alert('O código deve ter no mínimo 4 dígitos.');
+  } else {
+    window.location.href = 'nova-senha.html';
+  }
+});
+
