04bb4d611767ae416b62374408c79242e97b7042
mariareisx <mclarareis77@gmail.com>
2025-05-25 14:50:18 -0300

    ajuste js

--- src/assets/js/userprofile.js
+++ src/assets/js/userprofile.js
@@ -1,8 +1,17 @@
 // recupera os dados do localStorage para exibição
-    document.getElementById("name").innerText = localStorage.getItem("name") || "Nome não informado";
-    document.getElementById("bio").innerText = localStorage.getItem("bio") || "Bio não informada";
-    document.getElementById("birthdate").innerText =
-      localStorage.getItem("birthdate") || "Data de nascimento não informada";
-    document.getElementById("children").innerText =
-      localStorage.getItem("children") || "Número de filhos não informado";
-    document.getElementById("location").innerText = localStorage.getItem("location") || "Localização não informada";
+document.getElementById("name").innerText = localStorage.getItem("name") || "Nome não informado";
+document.getElementById("bio").innerText = localStorage.getItem("bio") || "Bio não informada";
+document.getElementById("birthdate").innerText =
+    localStorage.getItem("birthdate") || "Data de nascimento não informada";
+document.getElementById("children").innerText =
+    localStorage.getItem("children") || "Número de filhos não informado";
+document.getElementById("location").innerText = localStorage.getItem("location") || "Localização não informada";
+
+// exibe foto de perfil
+
+var profilePic = localStorage.getItem("profilePic");
+if (profilePic) {
+    document.getElementById("profilePreview").src = profilePic;
+} else {
+    document.getElementById("profilePreview").src = "../assets/images/no-photo.jpg";
+}
