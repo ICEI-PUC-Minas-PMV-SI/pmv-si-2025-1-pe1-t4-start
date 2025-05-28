75bdef46fefb34bd8bcf1bc6b74ba4a5895c8b3a
motocaz <gustavomottadeveloper@gmail.com>
2025-05-27 21:12:21 -0300

    Refactor password reset forms and styles for improved consistency and usability

--- src/assets/js/novasenha.js
+++ src/assets/js/novasenha.js
@@ -1,18 +1,18 @@
 const form = document.getElementById('form-login');
-const senhaInput = document.getElementById('senha nova');
+const senhaInput = document.getElementById('nova-senha');
 const confirmeInput = document.getElementById('confirme');
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault(); 
+  e.preventDefault();
   const senha = senhaInput.value;
   if (senha.length < 8) {
     alert('A senha deve ter no mínimo 8 caracteres!');
   } else if (senha !== confirmeInput.value) {
     alert('As senhas não coincidem!');
   } else {
-        alert('Senha redefinida com sucesso!'); 
-        window.location.href = 'index.html'; 
-      }
-    });
+    alert('Senha redefinida com sucesso!');
+    window.location.href = 'index.html';
+  }
+});
 
 
