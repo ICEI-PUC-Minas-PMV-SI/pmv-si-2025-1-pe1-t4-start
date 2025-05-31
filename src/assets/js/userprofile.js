d13162e263688e8178f47380ac9ff49d863879d5
mariareisx <mclarareis77@gmail.com>
2025-05-31 20:36:04 -0300

    update

--- src/assets/js/userprofile.js
+++ src/assets/js/userprofile.js
@@ -1,11 +1,16 @@
 // recupera os dados do localStorage para exibição
-document.getElementById("name").innerText = localStorage.getItem("name") || "Nome não informado";
-document.getElementById("bio").innerText = localStorage.getItem("bio") || "Bio não informada";
+document.getElementById("name").innerText =
+    localStorage.getItem("name") || "Nome não informado";
+document.getElementById("bio").innerText =
+    localStorage.getItem("bio") || "Bio não informada";
 document.getElementById("birthdate").innerText =
     localStorage.getItem("birthdate") || "Data de nascimento não informada";
 document.getElementById("children").innerText =
     localStorage.getItem("children") || "Número de filhos não informado";
-document.getElementById("location").innerText = localStorage.getItem("location") || "Localização não informada";
+document.getElementById("location").innerText =
+    localStorage.getItem("location") || "Localização não informada";
+document.getElementById("email").innerText =
+    localStorage.getItem("email") || "Email não informado";
 
 // exibe foto de perfil
 
@@ -23,14 +28,18 @@ function previewProfilePicture(event) {
     };
     reader.readAsDataURL(event.target.files[0]);
 }
+
+// salva os dados do perfil 
+
 function salvarPerfil(event) {
-    event.preventDefault(); // impede que envie pro servidor
+    event.preventDefault();
 
     const nome = document.getElementById('name').value;
     const bio = document.getElementById('bio').value;
     const dataNasc = document.getElementById('birthdate').value;
     const filhos = document.getElementById('children').value;
     const localizacao = document.getElementById('location').value;
+    const email = document.getElementById('email').value;
 
     const profilePic = document.getElementById('profilePreview').src;
     const formattedDate = new Date(dataNasc).toLocaleDateString('pt-BR');
@@ -41,8 +50,10 @@ function salvarPerfil(event) {
     localStorage.setItem('children', filhos);
     localStorage.setItem('location', localizacao);
     localStorage.setItem('profilePic', profilePic);
+    localStorage.setItem('email', email);
     window.location.href = "userprofile.html";
 }
+// botao para excluir perfil
 
 function excluirPerfil() {
     const confirmacao = confirm("Tem certeza que deseja excluir seu perfil?");
@@ -53,6 +64,7 @@ function excluirPerfil() {
         localStorage.removeItem("children");
         localStorage.removeItem("location");
         localStorage.removeItem("profilePic");
+        localStorage.removeItem("email");
         alert("Perfil excluído com sucesso.");
         window.location.href = "landing-page.html";
     } else {
