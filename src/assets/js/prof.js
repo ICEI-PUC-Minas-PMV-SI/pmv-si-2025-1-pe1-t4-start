f1b90b4d82f2f88900ee0657d517b631a59336c0
Laila Maria da Silva <laila.silva.profissionaltext@gmail.com>
2025-05-30 23:30:39 -0300

    Update prof.js

--- src/assets/js/prof.js
+++ src/assets/js/prof.js
@@ -1,17 +1,61 @@
-const form = document.getElementById('form-login');
-const senhaInput = document.getElementById('senha nova');
-const confirmeInput = document.getElementById('confirme');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault(); 
-  const senha = senhaInput.value;
-  if (senha.length < 8) {
-    alert('A senha deve ter no mínimo 8 caracteres!');
-  } else if (senha !== confirmeInput.value) {
-    alert('As senhas não coincidem!');
+const especialidadeInput = document.getElementById('especialidade');
+const registroContainer = document.getElementById('registroContainer');
+const registroInput = document.getElementById('registro');
+const locInput = document.getElementById('loc');
+const button = document.querySelector('.prof');
+
+const options = {
+  'Psicólogo': 'CRP:',
+  'Advogado': 'OAB:',
+  'Baba': null,
+  'Creche': null
+};
+
+function toggleRegistroInput() {
+  const especialidade = especialidadeInput.value;
+  if (especialidade === 'Psicólogo' || especialidade === 'Advogado') {
+    registroInput.value = options[especialidade] || '';
+    registroInput.required = true;
+    registroContainer.style.display = 'block'; 
   } else {
-        alert('Senha redefinida com sucesso!'); 
-        window.location.href = 'index.html'; 
-      }
-    });
+    registroInput.value = '';
+    registroInput.required = false;
+    registroContainer.style.display = 'none';  
+  }
+}
 
+especialidadeInput.addEventListener('input', toggleRegistroInput);
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  const especialidade = especialidadeInput.value;
+  const registro = registroInput.value;
+  const localizacao = locInput.value;
+
+  const especialidadesValidas = Object.keys(options);
+
+  if (!especialidadesValidas.includes(especialidade)) {
+    alert('Por favor, selecione uma das especialidades disponíveis!');
+    return;
+  }
+
+  if ((especialidade === 'Psicólogo' || especialidade === 'Advogado') && registro === '') {
+    alert('Por favor, preencha o registro profissional!');
+    return;
+  }
+
+  if ((especialidade === 'Psicólogo' || especialidade === 'Advogado') && !registro.match(/\d/)) {
+    alert('O registro profissional deve conter números!');
+    return;
+  }
+
+  if (localizacao === '') {
+    alert('Por favor, preencha o campo de cidade!');
+    return;
+  }
+
+  window.location.href = 'forum.html';
+});
+
+toggleRegistroInput();
